c872c5424c7cc39bf29cd6dd62c6db4e8b985429
Mika <stam@blabla.com>
2024-12-26 09:20:22 +0200

    commit

--- Client-React/app/src/App.js
+++ Client-React/app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Home from './Pages/Home'
 
+
 function App() {
   return (
 
--- Client-React/app/src/index.js
+++ Client-React/app/src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './modules/theme';
+import Home from './Pages/Home';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
     <App />
+    </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
