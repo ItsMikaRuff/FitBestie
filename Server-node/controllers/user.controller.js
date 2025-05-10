b9201d121282fbc024a7de4814edd003e201dc5e
adiellll4 <adieloosh8@gmail.com>
2025-05-10 23:04:32 +0300

    הוספת הצפנה ותוקן לסיסמת משתמש

--- Server-node/controllers/user.controller.js
+++ Server-node/controllers/user.controller.js
@@ -2,15 +2,17 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const userModel = require("../models/user.model");
 const trainerModel = require("../models/trainer.model");
 
 const createUser = async (data) => {
     try {
-        const confirmPassword = data.confirmPassword;  // נשמר לבד
+        const confirmPassword = data.confirmPassword; // קודם כל לשמור
+
         const userData = { ...data };
-        delete userData.confirmPassword;  // מוסר לפני ה־create
+        delete userData.confirmPassword;
 
         const missingFields = [];
 
@@ -18,24 +20,31 @@ const createUser = async (data) => {
         if (!userData.email) missingFields.push('אימייל');
         if (!userData.password) missingFields.push('סיסמה');
         if (!confirmPassword) missingFields.push('אימות סיסמה');
-        if (!userData.phone) missingFields.push('מספר טלפון');
-
-        if (userData.role === 'trainer') {
-            if (!userData.paymentDetails?.cardNumber) missingFields.push('מספר כרטיס אשראי');
-            if (!userData.paymentDetails?.expiryDate) missingFields.push('תוקף כרטיס אשראי');
-            if (!userData.paymentDetails?.cvv) missingFields.push('קוד CVV');
-        }
 
         if (missingFields.length > 0) {
             throw new Error(`נא למלא את השדות הבאים: ${missingFields.join(', ')}`);
         }
 
-        if (confirmPassword !== userData.password) {
+        if (userData.password !== confirmPassword) {
             throw new Error("אימות הסיסמה אינו תואם");
         }
 
+        // Hash the password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
+        userData.password = hashedPassword;
+
         let createdUser;
         if (userData.role === 'trainer') {
+            if (!userData.paymentDetails?.cardNumber) missingFields.push('מספר כרטיס אשראי');
+            if (!userData.paymentDetails?.expiryDate) missingFields.push('תוקף כרטיס אשראי');
+            if (!userData.paymentDetails?.cvv) missingFields.push('קוד CVV');
+            if (!userData.phone) missingFields.push('מספר טלפון');
+
+            if (missingFields.length > 0) {
+                throw new Error(`נא למלא את השדות הבאים: ${missingFields.join(', ')}`);
+            }
+
             createdUser = await trainerModel.create(userData);
         } else {
             createdUser = await userModel.create(userData);
@@ -67,13 +76,14 @@ const read = async (filter = {}) => {
 const update = async (filter, data) => {
     try {
         // נזהה האם המשתמש הוא מאמן או רגיל
-        const baseModel = filter.role === 'trainer' ? trainerModel : userModel;
+        const user = await userModel.findById(filter._id);
+        const baseModel = user.role === 'trainer' ? trainerModel : userModel;
 
-        const user = await baseModel.findByIdAndUpdate(
-            filter._id,
-            { $set: data },
-            { new: true, runValidators: true }
-        );
+        // const user = await baseModel.findByIdAndUpdate(
+        //     filter._id,
+        //     { $set: data },
+        //     { new: true, runValidators: true }
+        // );
 
         console.log("User updated successfully:", user);
         return user;
@@ -125,3 +135,4 @@ module.exports = {
     searchByLocation,
     searchByTypeAndLocation
 };
+
