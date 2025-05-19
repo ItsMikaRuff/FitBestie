1afd0763b228d96434fb7327fcde8d277f2ea70d
adiellll4 <adieloosh8@gmail.com>
2025-05-19 22:05:58 +0300

    סיום דמות וסרטוני כושר

--- Client-React/app/src/pages/WorkoutVideos.jsx
+++ Client-React/app/src/pages/WorkoutVideos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef} from 'react';
 import styled from 'styled-components';
 import HumanBody from '../components/HumanBody';
 
@@ -19,9 +19,8 @@ const Title = styled.h1`
 
 const ModelContainer = styled.div`
   width: 70%;
-  background: #f0f0f0;
+  background: #f5f5f5;
   border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   margin-bottom: 2rem;
 `;
 
@@ -59,6 +58,7 @@ const SelectedMuscle = styled.div`
 `;
 
 const WorkoutVideos = () => {
+  const videoSectionRef = useRef(null);
   const [selectedMuscle, setSelectedMuscle] = useState(null);
 
   const videos = {
@@ -69,13 +69,10 @@ const WorkoutVideos = () => {
       { id: 4, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/eJMy9LglyF0' }
     ],
     'front-arm-right': [
-      { id: 1, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/sLGKDQAESGo?start=1' },
-      { id: 2, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/zdmZYCGkCgU?start=1' },
-      { id: 3, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/I-h9DdLxq_k' },
-      { id: 4, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/eJMy9LglyF0' }
-    ],
-    'back-arm-left': [
-      { id: 5, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/I-h9DdLxq_k' }
+      { id: 74, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/sLGKDQAESGo?start=1' },
+      { id: 75, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/zdmZYCGkCgU?start=1' },
+      { id: 76, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/I-h9DdLxq_k' },
+      { id: 77, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/eJMy9LglyF0' }
     ],
     'front-shoulder-left': [
       { id: 9, title: 'אימון כתפיים', url: ' https://www.youtube.com/embed/47sFXBoSx8M?start=1' },
@@ -83,12 +80,9 @@ const WorkoutVideos = () => {
       { id: 11, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/rb4S9UND-Wc' },
     ],
     'front-shoulder-right':[
-      { id: 9, title: 'אימון כתפיים', url: ' https://www.youtube.com/embed/47sFXBoSx8M?start=1' },
-      { id: 10, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/2Xzx51BJVys?start=2' },
-      { id: 11, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/rb4S9UND-Wc' },
-    ],
-    'back-shoulder-left': [
-      { id: 7, title: 'אימון כתפיים אחוריות', url: 'https://www.youtube.com/embed/rb4S9UND-Wc' }
+      { id: 12, title: 'אימון כתפיים', url: ' https://www.youtube.com/embed/47sFXBoSx8M?start=1' },
+      { id: 13, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/2Xzx51BJVys?start=2' },
+      { id: 14, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/rb4S9UND-Wc' },
     ],
     'chest': [
       { id: 15, title: 'אימון בית החזה', url: 'https://www.youtube.com/embed/B8EfL9JNmM4?start=6' },
@@ -125,67 +119,83 @@ const WorkoutVideos = () => {
       { id: 37, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/MZWq7Kx-5FQ?start=14' },
     ],
     'front-thigh-right':[
-      { id: 32, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/p7mswiVqSaU?start=1' },
-      { id: 33, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/Bs0FmuqUzwU?start=44' },
-      { id: 34, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/3VvZkGvrBKY?start=236' },
-      { id: 35, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/a2UnibbfW7s?start=1' },
-      { id: 36, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/vO9Pig-9Xzc' },
-      { id: 37, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/MZWq7Kx-5FQ?start=14' },
+      { id: 38, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/p7mswiVqSaU?start=1' },
+      { id: 39, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/Bs0FmuqUzwU?start=44' },
+      { id: 40, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/3VvZkGvrBKY?start=236' },
+      { id: 41, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/a2UnibbfW7s?start=1' },
+      { id: 42, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/vO9Pig-9Xzc' },
+      { id: 43, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/MZWq7Kx-5FQ?start=14' },
+    ],
+    'front-inner-thighs':[
+      { id: 44, title: 'תרגילי ירך פנימית', url: 'https://www.youtube.com/embed/o8_BR3AWvRM' },
+      { id: 45, title: 'תרגילי ירך פנימית', url: 'https://www.youtube.com/embed/pT8YHR0G_CU' },
+      { id: 46, title: 'תרגילי ירך פנימית', url: 'https://www.youtube.com/embed/3mm5pMNFLr4' },
+    ],
+    'calf-right':[
+      { id: 47, title: 'תרגילי ארבע ראשי', url: 'https://www.youtube.com/embed/XhlpzVXpbiE' },
+      { id: 48, title: 'תרגילי ארבע ראשי', url: 'https://www.youtube.com/embed/AK1LLP_d1Mk' },
+      { id: 49, title: 'תרגילי ארבע ראשי', url: 'https://www.youtube.com/embed/BePwGQkTq2U' },
+    ],
+    'calf-left':[
+      { id: 50, title: 'תרגילי ארבע ראשי', url: 'https://www.youtube.com/embed/XhlpzVXpbiE' },
+      { id: 51, title: 'תרגילי ארבע ראשי', url: 'https://www.youtube.com/embed/AK1LLP_d1Mk' },
+      { id: 52, title: 'תרגילי ארבע ראשי', url: 'https://www.youtube.com/embed/BePwGQkTq2U' },
     ],
     'back-thigh-left': [
-      { id: 12, title: 'תרגילי יריכיים אחוריות', url: 'https://www.youtube.com/embed/MZWq7Kx-5FQ?start=14' }
+      { id: 53, title: 'תרגילי ירך אחורית', url: 'https://www.youtube.com/embed/4GjFXO0iXCc' },
+      { id: 54, title: 'תרגילי ירך אחורית', url: 'https://www.youtube.com/embed/0vyCrSd1sqw' },
+      { id: 55, title: 'תרגילי ירך אחורית', url: 'https://www.youtube.com/embed/8_1enCMxNnI' },
+      ],
+'back-thigh-right': [
+      { id: 56, title: 'תרגילי ירך אחורית', url: 'https://www.youtube.com/embed/4GjFXO0iXCc' },
+      { id: 57, title: 'תרגילי ירך אחורית', url: 'https://www.youtube.com/embed/0vyCrSd1sqw' },
+      { id: 58, title: 'תרגילי ירך אחורית', url: 'https://www.youtube.com/embed/8_1enCMxNnI' },
     ],
-    'front-knee-left': [
-      { id: 45, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/-vdtoAKDQWY?start=64' },
-      { id: 46, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/mjNWpbTwiq4?start=171' },
-      { id: 47, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/GU8d7NdWsZo?start=1' },
+    'glutes': [
+      { id: 59, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/4GvXn4u2P_M' },
+      { id: 60, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/5yYll5MHqOE' },
+      { id: 61, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/U6qUoloR8T8' },
+      { id: 62, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/vO9Pig-9Xzc' },
+      { id: 63, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/RaJO71A4E6I' },
+      { id: 64, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/dCHQr5c0efs' },
     ],
-    'front-knee-right':[
-      { id: 45, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/-vdtoAKDQWY?start=64' },
-      { id: 46, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/mjNWpbTwiq4?start=171' },
-      { id: 47, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/GU8d7NdWsZo?start=1' },
-    ],
-    'back-knee-left': [
-      { id: 14, title: 'חיזוק ברכיים אחוריות', url: 'https://www.youtube.com/embed/GU8d7NdWsZo?start=1' }
-    ],
-    'front-foot-left': [
-      { id: 51, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/xTUmZcxf_tI?start=1' },
-      { id: 52, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/5k9k_tioGSo' },
-      { id: 53, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/gwZqWjrYSNM' },
-    ],
-    'front-foot-right':[
-      { id: 51, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/xTUmZcxf_tI?start=1' },
-      { id: 52, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/5k9k_tioGSo' },
-      { id: 53, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/gwZqWjrYSNM' },
-    ],
-    'back-foot-left': [
-      { id: 16, title: 'כף רגל אחורית', url: 'https://www.youtube.com/embed/gwZqWjrYSNM' }
-    ],
+    
     back: [
-      { id: 17, title: 'אימון גב', url: 'https://www.youtube.com/embed/EF0f4yCMiDU' }
+      { id: 65, title: 'אימון גב', url: 'https://www.youtube.com/embed/8VdwJWLGa5k' },
+      { id: 66, title: 'אימון גב', url: 'https://www.youtube.com/embed/t8EjI-R0pcQ' },
+      { id: 67, title: 'אימון גב', url: 'https://www.youtube.com/embed/o062bxRT1EA' },
     ],
-    glutes: [
-      { id: 18, title: 'אימון ישבן', url: 'https://www.youtube.com/embed/1BZM9U_EKZk' }
-    ]
+    'back-arm-left': [
+      { id: 68, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/2Xzx51BJVys' },
+      { id: 69, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/gJe9ECgDrkk' },
+      { id: 70, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/njWxT1A7XrY' },
+    ],
+    'back-arm-right': [
+      { id: 71, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/2Xzx51BJVys' },
+      { id: 72, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/gJe9ECgDrkk' },
+      { id: 73, title: 'אימון זרוע אחורית', url: 'https://www.youtube.com/embed/njWxT1A7XrY' },
+    ],
   };
 
   const handleMuscleClick = (name) => {
     setSelectedMuscle(name);
   };
 
-  const handleMuscleHover = (name) => {
-    console.log('Hovered:', name);
-  };
+  setTimeout(() => {
+    videoSectionRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, 100);
 
   return (
     <Container>
-      <Title>Workout Videos</Title>
+     <Title>תרגילי כושר לפי חלקי גוף</Title>
+     <h2>לחצי על אזור מסוים בגוף כדי לצפות בסרטוני אימון מותאמים</h2>
 
       <ModelContainer>
-        <HumanBody onMuscleClick={handleMuscleClick} onMuscleHover={handleMuscleHover} />
+        <HumanBody onMuscleClick={handleMuscleClick} />
       </ModelContainer>
 
       {selectedMuscle && (
+         <div ref={videoSectionRef}>
         <>
           <SelectedMuscle>תרגילים:</SelectedMuscle>
           {videos[selectedMuscle]?.length > 0 ? (
@@ -209,6 +219,7 @@ const WorkoutVideos = () => {
             <p>No videos available for this muscle group yet.</p>
           )}
         </>
+        </div>
       )}
     </Container>
   );
@@ -234,210 +245,3 @@ export default WorkoutVideos;
 
 
 
-// import React, { useState } from 'react';
-// import styled from 'styled-components';
-// import HumanBody from '../components/HumanBody';
-
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   padding: 2rem;
-//   min-height: 100vh;
-//   background: #f5f5f5;
-// `;
-
-// const Title = styled.h1`
-//   color: #333;
-//   margin-bottom: 2rem;
-//   text-align: center;
-// `;
-
-// const ModelContainer = styled.div`
-//   width: 70%;
-//   background: #f0f0f0;
-//   border-radius: 10px;
-//   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-//   margin-bottom: 2rem;
-// `;
-
-// const VideoGallery = styled.div`
-//   display: grid;
-//   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-//   gap: 2rem;
-//   width: 100%;
-//   max-width: 1200px;
-// `;
-
-// const VideoCard = styled.div`
-//   background: white;
-//   border-radius: 10px;
-//   overflow: hidden;
-//   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-//   transition: transform 0.2s;
-
-//   &:hover {
-//     transform: translateY(-5px);
-//   }
-// `;
-
-// const VideoTitle = styled.h3`
-//   padding: 1rem;
-//   margin: 0;
-//   color: #333;
-// `;
-
-// const SelectedMuscle = styled.div`
-//   font-size: 1.2rem;
-//   color: #666;
-//   margin-bottom: 1rem;
-//   text-align: center;
-// `;
-
-// const WorkoutVideos = () => {
-//   const [selectedMuscle, setSelectedMuscle] = useState(null);
-
-//   const videos = {
-//     'arm-left': [
-//       { id: 1, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/sLGKDQAESGo?start=1'},
-//       { id: 2, title: ' אימון זרוע קידמית', url: 'https://www.youtube.com/embed/zdmZYCGkCgU?start=1'},
-//       { id: 3, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/I-h9DdLxq_k'},
-//       { id: 4, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/eJMy9LglyF0'},
-
-//     ],
-//     'arm-right': [
-//       { id: 5, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/sLGKDQAESGo?start=1'},
-//       { id: 6, title: ' אימון זרוע קידמית', url: 'https://www.youtube.com/embed/zdmZYCGkCgU?start=1'},
-//       { id: 7, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/I-h9DdLxq_k'},
-//       { id: 8, title: 'אימון זרוע קידמית', url: 'https://www.youtube.com/embed/eJMy9LglyF0'},
-//     ],
-//     'shoulder-left': [
-//       { id: 9, title: 'אימון כתפיים', url: ' https://www.youtube.com/embed/47sFXBoSx8M?start=1' },
-//       { id: 10, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/2Xzx51BJVys?start=2' },
-//       { id: 11, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/rb4S9UND-Wc' },
-//     ],
-//     'shoulder-right': [
-//       { id: 12, title: 'אימון כתפיים', url: ' https://www.youtube.com/embed/47sFXBoSx8M?start=1' },
-//       { id: 13, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/2Xzx51BJVys?start=2' },
-//       { id: 14, title: 'אימון כתפיים', url: 'https://www.youtube.com/embed/rb4S9UND-Wc' },
-//     ],
-//     'chest': [
-//       { id: 15, title: 'אימון בית החזה', url: 'https://www.youtube.com/embed/B8EfL9JNmM4?start=6' },
-//       { id: 16, title: 'אימון בית החזה', url: 'https://www.youtube.com/embed/V9lvyOIDAII?start=110' },
-//       { id: 17, title: 'אימון בית החזה', url: 'https://www.youtube.com/embed/1phjKdK18lA' },
-//       { id: 18, title: 'אימון בית החזה', url: 'https://www.youtube.com/embed/wUOBow4rsSY' },
-//       { id: 19, title: 'אימון בית החזה', url: 'https://www.youtube.com/embed/qKxpVD8K3zY?start=1' },
-//     ],
-//     'abs': [
-//       { id: 20, title: 'אימון בטן', url: 'https://www.youtube.com/embed/2pLT-olgUJs?start=1' },
-//       { id: 21, title: 'אימון בטן', url: 'https://www.youtube.com/embed/zGITL_k1D2U?start=1' },
-//       { id: 22, title: 'אימון בטן', url: 'https://www.youtube.com/embed/FQbnzb6IIbY' },
-//       { id: 23, title: 'אימון בטן', url: 'https://www.youtube.com/embed/1f8yoFFdkcY?start=1' },
-//       { id: 24, title: 'אימון בטן', url: 'https://www.youtube.com/embed/x33O9qGN_us' },
-//       { id: 25, title: 'אימון בטן', url: 'https://www.youtube.com/embed/YQPrLZIxosA?start=2' },
-
-//     ],
-//     'oblique-left': [
-//       { id: 26, title: 'אימון בטן צדדית', url: 'https://www.youtube.com/embed/FQbnzb6IIbY' },
-//       { id: 27, title: 'אימון בטן צדדית', url: 'https://www.youtube.com/embed/TF9-zVUQoSI' },
-//       { id: 28, title: 'אימון בטן צדדית', url: 'https://www.youtube.com/embed/KWSkT74zJ5U' },
-//     ],
-//     'oblique-right': [
-//       { id: 29, title: 'אימון בטן צדדית', url: 'https://www.youtube.com/embed/FQbnzb6IIbY' },
-//       { id: 30, title: 'אימון בטן צדדית', url: 'https://www.youtube.com/embed/TF9-zVUQoSI' },
-//       { id: 31, title: 'אימון בטן צדדית', url: 'https://www.youtube.com/embed/KWSkT74zJ5U' },
-//     ],
-//     // back: [
-//     //   { id: 9, title: 'Back Workout', url: 'https://www.youtube.com/embed/EF0f4yCMiDU' },
-//     // ],
-//     // glutes: [
-//     //   { id: 10, title: 'Glute Workout', url: 'https://www.youtube.com/embed/1BZM9U_EKZk' },
-//     // ],
-//     'thigh-left': [
-//       { id: 32, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/p7mswiVqSaU?start=1' },
-//       { id: 33, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/Bs0FmuqUzwU?start=44' },
-//       { id: 34, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/3VvZkGvrBKY?start=236' },
-//       { id: 35, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/a2UnibbfW7s?start=1' },
-//       { id: 36, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/vO9Pig-9Xzc' },
-//       { id: 37, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/MZWq7Kx-5FQ?start=14' },
-//     ],
-//     'thigh-right': [
-//       { id: 38, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/p7mswiVqSaU?start=1' },
-//       { id: 39, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/Bs0FmuqUzwU?start=44' },
-//       { id: 40, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/3VvZkGvrBKY?start=236' },
-//       { id: 42, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/a2UnibbfW7s?start=1' },
-//       { id: 43, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/vO9Pig-9Xzc' },
-//       { id: 44, title: 'תרגילי יריכיים', url: 'https://www.youtube.com/embed/MZWq7Kx-5FQ?start=14' },
-//     ],
-//     'knee-left': [
-//       { id: 45, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/-vdtoAKDQWY?start=64' },
-//       { id: 46, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/mjNWpbTwiq4?start=171' },
-//       { id: 47, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/GU8d7NdWsZo?start=1' },
-//     ],
-//     'knee-right': [
-//       { id: 48, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/-vdtoAKDQWY?start=64' },
-//       { id: 49, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/mjNWpbTwiq4?start=171' },
-//       { id: 50, title: 'חיזוק שרירי ברכיים', url: 'https://www.youtube.com/embed/GU8d7NdWsZo?start=1' },
-//     ],
- 
-//     'foot-left': [
-//       { id: 51, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/xTUmZcxf_tI?start=1' },
-//       { id: 52, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/5k9k_tioGSo' },
-//       { id: 53, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/gwZqWjrYSNM' },
-//     ],
-//     'foot-right': [
-//       { id: 51, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/xTUmZcxf_tI?start=1' },
-//       { id: 52, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/5k9k_tioGSo' },
-//       { id: 53, title: 'חיזוק כף הרגל', url: 'https://www.youtube.com/embed/gwZqWjrYSNM' },
-//     ],
-//   };
-
-//   const handleMuscleClick = (name) => {
-//     setSelectedMuscle(name);
-//   };
-
-//   const handleMuscleHover = (name) => {
-//     console.log('Hovered:', name);
-//   };
-
-//   return (
-//     <Container>
-//       <Title>Workout Videos</Title>
-
-//       <ModelContainer>
-//         <HumanBody
-//           onMuscleClick={handleMuscleClick}
-//           onMuscleHover={handleMuscleHover}
-//         />
-//       </ModelContainer>
-
-//       {selectedMuscle && (
-//         <>
-//           <SelectedMuscle>תרגילים:</SelectedMuscle>
-//           {videos[selectedMuscle]?.length > 0 ? (
-//             <VideoGallery>
-//               {videos[selectedMuscle].map((video) => (
-//                 <VideoCard key={video.id}>
-//                   <iframe
-//                     width="100%"
-//                     height="200"
-//                     src={video.url}
-//                     title={video.title}
-//                     frameBorder="0"
-//                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//                     allowFullScreen
-//                   />
-//                   <VideoTitle>{video.title}</VideoTitle>
-//                 </VideoCard>
-//               ))}
-//             </VideoGallery>
-//           ) : (
-//             <p>No videos available for this muscle group yet.</p>
-//           )}
-//         </>
-//       )}
-//     </Container>
-//   );
-// };
-
-// export default WorkoutVideos;
